f268da44ae154bde2afd520b2a9dc69daefe972b
Phillip Lange <langepj@gmail.com>
2018-05-04 07:40:45 +1200

    Project team initial commit

---
+++ avc_code/main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+#define  CAMERA_MAX_X 320
+#define  CAMERA_MAX_Y 240
+
+
+int get_direction_from_camera(){
+	int dir = 0, total = 0;
+	take_picture();
+	for(int x = 0; x < CAMERA_MAX_X; x++){
+		for(int y = 0; y < CAMERA_MAX_Y; y++){
+			if(get_pixel(x, y, 3) > 155){
+				total++;
+				if(x > (CAMERA_MAX_X / 2) )
+					dir++;
+				else
+					dir--;
+			}
+		}
+	}
+	if (total < 10) dir = 90;
+	return dir;
+}
+
+int main(int argc, char **argv){
+    init();
+
+
+	return 0;
+}
