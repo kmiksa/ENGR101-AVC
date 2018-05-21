252c96640c14cfbd2753a3122690264e41dccee9
Jakub Miksa <miksa.kuba@gmail.com>
2018-05-21 15:50:18 +1200

    Add files via upload

--- AVC/avcpn.cpp
+++ AVC/avcpn.cpp
@@ -6,7 +6,16 @@
 #define CAMERA_MAX_Y  240
 #define LEFT_MOTOR_PIN 1
 #define RIGHT_MOTOR_PIN 2
+#define FRONT_SENSOR 0
+#define LEFT_SENSOR 1
+#define RIGHT_SENSOR 2
 
+
+
+void dead_stop(){
+        set_motor(1, 0);
+        set_motor(2, 0);
+}
 
 void move(int error){
 	int Kp = 0.005; // 0.005,0.05 test it bitch !
@@ -16,6 +25,8 @@ void move(int error){
 	int v_right = forward_speed - difference; 
 	set_motor(LEFT_MOTOR_PIN, v_left);
 	set_motor(RIGHT_MOTOR_PIN, v_right);
+	sleep1(0,10000);
+	dead_stop();
 }
 
 int calculate_white_threshold(){
@@ -61,19 +72,34 @@ int measure_error(int white_threshold){
 		}
 	
 }
+int find_red(){
+	int max = 0;
+	for(int i = 0; i < 320; i++){
+		int redPix = get_pixel(120,i,1);
+		if(redPix > max){
+			max = redPix;
+			}
+		}
+		return max;
+	}
 
-void dead_stop(){
-        set_motor(1, 0);
-        set_motor(2, 0);
-}
+int measure_errorl(){
+		int error = 0;
+	
+		int left_sensor = -1*(read_analog(LEFT_SENSOR));
+		int right_sensor = read_analog(RIGHT_SENSOR);
+		return error = left_sensor + right_sensor;
+	}
 
-void move_forward(){
+
+
+/**void move_forward(){
         set_motor(1,15*1.5);
         set_motor(2,75*1.68);
         sleep1(0 , 10000);
         dead_stop();
 
-}
+}*/
 
 int scan_line(int sx, int ex, int sy, int ey){ //start x, end x, start y, end  y 
     take_picture();
@@ -95,44 +121,58 @@ int scan_line(int sx, int ex, int sy, int ey){ //start x, end x, start y, end  y
     return max; 
 }
 
-void get_corners(int white_threshold){
+void get_corners(int white_threshold, int error){
     take_picture();
     
-    int error = measure_error(white_threshold);
     int yl = scan_line(20,20,0,CAMERA_MAX_Y);
     int yr = scan_line(CAMERA_MAX_X-20,CAMERA_MAX_X-20,0,CAMERA_MAX_Y);
     
-    while(error > -10000 && error < 10000) {
-        error = measure_error(white_threshold);
+    
+    if(error > -10000 && error < 10000) {
         move(error);
-     if((error > -10000 && error < 10000) && (yl > white_threshold && yr > white_threshold))  {
-      move_forward();
+	}
+    if((error == -10000) && (yl > white_threshold && yr > white_threshold))  {
+      //move_forward();
       while(error != 0){
-		  //turn right 
-		  set_motor(1,75*1.5);
+		  //turn left 
+		  set_motor(1,-75*1.5);
 		  set_motor(2,75*1.68);
 		  }
-    } else{
+    } 
+    if((error == -10000) && (yl < white_threshold && yr < white_threshold)){
         //dead end turn back 
-        move_forward();
+        //move_forward();
 		while(error != 0){
-		  //turn right 
-		  set_motor(1,75*1.5);
+		  //turn left 
+		  set_motor(1,-75*1.5);
 		  set_motor(2,75*1.68);
 		  }
     }
-}
+    if((error == -10000) && (yl < white_threshold && yr > white_threshold)){
+		//turn right
+		  while(error != 0){
+		  set_motor(1,75*1.5);
+		  set_motor(2,-75*1.68);
+		  }
+		}
+	if((error == -10000) && (yl > white_threshold && yr < white_threshold)){
+		//turn left
+		while(error != 0){
+		  set_motor(1,-75*1.5);
+		  set_motor(2,75*1.68);
+		  }
+		}
+
 }
 
 int main(){
 	init();
 	
-	int quadrant = 0;
+	int quadrant = 1;
 	while(1){
 		if(quadrant == 0){
 			connect_to_server("130.195.6.196", 1024);
-			send_to_server("Please");
-			char pwd[24];
+			char pwd[24] = {'P','l','e','a','s','e'};
 			receive_from_server(pwd);
 			send_to_server(pwd);
 			sleep1(0, 5000);
@@ -148,8 +188,11 @@ int main(){
 			}
 		if(quadrant == 2){
 			int white_threshold = calculate_white_threshold();
-			get_corners(white_threshold);
-			
+			int error = measure_error(white_threshold);
+			get_corners(white_threshold, error);
+			if(find_red() > 100) {
+				quadrant ++;
+				}
 			}	
 		}
 	}
